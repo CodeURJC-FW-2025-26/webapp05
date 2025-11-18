14a0a4d9e73d42cc235ceae023c85be30ff66713
javrodr19 <rodiljv1984@gmail.com>
2025-11-18 16:04:49 +0100

    footer y header y reseñas

--- src/load_data.js
+++ src/load_data.js
@@ -5,18 +5,31 @@ const UPLOADS_FOLDER = './uploads';
 const DATA_FOLDER = './data';
 
 let dataFile = 'data.json';
+let reviewsFile = 'reviews.json';
 
 const dataString = await fs.readFile(DATA_FOLDER + '/' + dataFile, 'utf8');
+const reviewsString = await fs.readFile(DATA_FOLDER + '/' + reviewsFile, 'utf8');
 
 const posts = JSON.parse(dataString);
+const reviewsTemplate = JSON.parse(reviewsString);
 
 await board.deletePosts();
+
 for(let post of posts){
-    await board.addPost(post);
+    const result = await board.addPost(post);
+    const postId = result.insertedId;
+
+    for (let review of reviewsTemplate) {
+        await board.addReview({
+            ...review, 
+            date: new Date().toISOString().split('T')[0],
+            postId: postId
+        });
+    }
 }
 
 await fs.rm(UPLOADS_FOLDER, { recursive: true, force: true });
 await fs.mkdir(UPLOADS_FOLDER);
 await fs.cp(DATA_FOLDER + '/images', UPLOADS_FOLDER, { recursive: true });
 
-console.log('Demo data loaded');
+console.log('Demo data loaded with reviews from JSON');
